e9d662ba046512ea6c8d6da2503eaea8633af86d
alexiosgrig <alexios.grig@gmail.com>
2022-01-18 23:24:56 +0200

    commit

---
+++ build/precache-manifest.47883106a8aff153f0482491b9dde7c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b86407f8c00b1aeef42d2b24bbc85554",
+    "url": "/bullmarket/index.html"
+  },
+  {
+    "revision": "7fbcd2a2d7d5bad0516a",
+    "url": "/bullmarket/static/css/main.d0268953.chunk.css"
+  },
+  {
+    "revision": "b580c61e03b4ac4c5e8f",
+    "url": "/bullmarket/static/js/2.469a0cef.chunk.js"
+  },
+  {
+    "revision": "7fbcd2a2d7d5bad0516a",
+    "url": "/bullmarket/static/js/main.c27c2c73.chunk.js"
+  },
+  {
+    "revision": "80740c4cb81a1e50c65f",
+    "url": "/bullmarket/static/js/runtime~main.501194e9.js"
+  }
+]);

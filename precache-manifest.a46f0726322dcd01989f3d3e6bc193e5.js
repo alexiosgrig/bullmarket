d30a7f5f4e95c52753d61c088da8e56e45912960
alexiosgrig <alexios.grig@gmail.com>
2022-01-19 10:05:53 +0200

    Updates

--- precache-manifest.a46f0726322dcd01989f3d3e6bc193e5.js
+++ precache-manifest.a46f0726322dcd01989f3d3e6bc193e5.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dbe1709799f0daab90bc46cc2bc54e2a",
+    "revision": "8ad3deffb1a52ae03e190ac389a52801",
     "url": "/bullmarket/index.html"
   },
   {
-    "revision": "38e4cd8cec1b751e543f",
+    "revision": "0a6e55b14dd0101993a9",
     "url": "/bullmarket/static/css/main.d0268953.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/bullmarket/static/js/2.469a0cef.chunk.js"
   },
   {
-    "revision": "38e4cd8cec1b751e543f",
-    "url": "/bullmarket/static/js/main.84e7de26.chunk.js"
+    "revision": "0a6e55b14dd0101993a9",
+    "url": "/bullmarket/static/js/main.205c56be.chunk.js"
   },
   {
     "revision": "80740c4cb81a1e50c65f",

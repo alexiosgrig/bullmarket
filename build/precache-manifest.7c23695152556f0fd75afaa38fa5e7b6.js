417b605465f077365de889efdd09c5fb6341f86d
alexiosgrig <alexios.grig@gmail.com>
2022-01-23 23:31:44 +0200

    New Components

---
+++ build/precache-manifest.7c23695152556f0fd75afaa38fa5e7b6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2dc819ebe4ddfdccd90a206178ed5b4",
+    "url": "/bullmarket/index.html"
+  },
+  {
+    "revision": "88c2e6b5670eae0b87f2",
+    "url": "/bullmarket/static/css/2.124766c4.chunk.css"
+  },
+  {
+    "revision": "b996135bf6892438158a",
+    "url": "/bullmarket/static/css/main.5e057ebf.chunk.css"
+  },
+  {
+    "revision": "88c2e6b5670eae0b87f2",
+    "url": "/bullmarket/static/js/2.c61a3315.chunk.js"
+  },
+  {
+    "revision": "b996135bf6892438158a",
+    "url": "/bullmarket/static/js/main.4ad018c1.chunk.js"
+  },
+  {
+    "revision": "80740c4cb81a1e50c65f",
+    "url": "/bullmarket/static/js/runtime~main.501194e9.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/bullmarket/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/bullmarket/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/bullmarket/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/bullmarket/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/bullmarket/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
